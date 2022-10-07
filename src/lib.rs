81e9d7d68caccad0c27a1c51345ec49dcd3864d9
Aatif Syed <aatifsyedyp@gmail.com>
2022-10-07 20:49:28 +0100

    add SecretKey type

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+use anyhow::ensure;
+
+pub struct SecretKey<'a> {
+    buffer: &'a [u8],
+}
+
+impl<'a> TryFrom<&'a [u8]> for SecretKey<'a> {
+    type Error = anyhow::Error;
+
+    fn try_from(buffer: &'a [u8]) -> Result<Self, Self::Error> {
+        let num_bytes = buffer.len();
+        ensure!(
+            num_bytes <= 255,
+            "secret key length of {num_bytes} is too large"
+        );
+        Ok(Self { buffer })
+    }
+}
+
 /*
  * This function should return a cipher text for a given key and plaintext
  *
  */
-pub fn encode(key: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8> {
+pub fn encode(key: SecretKey, plaintext: Vec<u8>) -> Vec<u8> {
     let mut ciphertext = Vec::new();
     ciphertext
 }
@@ -11,7 +30,7 @@ pub fn encode(key: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8> {
  * This function should return a plaintext for a given key and ciphertext
  *
  */
-pub fn decode(key: Vec<u8>, ciphertext: Vec<u8>) -> Vec<u8> {
+pub fn decode(key: SecretKey, ciphertext: Vec<u8>) -> Vec<u8> {
     let mut plaintext = Vec::new();
     plaintext
 }
